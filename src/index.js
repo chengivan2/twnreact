6e8b92854d114b56b2080b4afe90f810033e174d
unknown <chengoleivan@gmail.com>
2022-04-18 02:06:33 +0300

    Second commit learning Git with Tailwind and React

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const header = <h1 className="text-white bg-black text-center py-7">BLACK IV</h1>;
+const header = <h1 className="text-white bg-black text-center py-6">BLACK IV</h1>;
 ReactDOM.render(header, document.getElementById('root'));
