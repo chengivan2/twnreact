a3e9742359e0fd13c2411eac4a95ff7ad6382607
unknown <chengoleivan@gmail.com>
2022-04-18 02:02:11 +0300

    First commit learning Git with Tailwind and React

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const header = <h1 className="text-white bg-black text-center py-7">BLACK IV</h1>;
+ReactDOM.render(header, document.getElementById('root'));
