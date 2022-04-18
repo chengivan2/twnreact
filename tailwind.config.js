5ebf76b9d268f80efa8b1c0901948c9c6388a639
unknown <chengoleivan@gmail.com>
2022-04-18 15:13:42 +0300

    At this point I will have to restart a new repository to freshly configure everything properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
+  
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
